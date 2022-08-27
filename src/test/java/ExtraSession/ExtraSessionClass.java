59c7cef36f2a93b6711cc5dab24488bc890d720b
babarzai <babarzai@gmail.com>
2022-08-27 12:41:17 -0400

    New session gitBash

---
+++ src/test/java/ExtraSession/ExtraSessionClass.java
@@ -0,0 +1,14 @@
+package ExtraSession;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ExtraSessionClass {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+
+        System.out.println("Extra Session GitBash - first commit");
+    }
+}
